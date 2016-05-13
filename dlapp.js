b4a89188d734d320ec4e5585f52aa8447ec0a886
Joel Barna <joel.tbarna@gmail.com>
2016-05-13 01:41:53 -0400

    Updated dlapp to grab the JavaGUI.jar file directly

--- dlapp.js
+++ dlapp.js
@@ -4,14 +4,12 @@ var fs = require('fs'),
     exec = require('child_process').execSync,
 	https = require('https');
 
-/* This script makes https github api requests to get the latest
-* release of both 
+/* This script makes https github api requests to get the latest release of  
 * FlyingOranger/FlyingOranger
 * FlyingOranger/JavaGUI
 *
-* If there is a new release, it delets the old folders,
-* it downloads the new release as a zip file
-* And then unzips it.
+* If there is a new release, it delets the old folders / files
+* it downloads the new release
 *
 * If we're installing, then we also download the FlyingOranger/notification_apps repository
 */
@@ -29,6 +27,11 @@ module.exports = cb => {
             if (!fs.existsSync( path.join( __dirname, "Notification_Apps"))){
                 
                 downloader('https://github.com/FlyingOranger/Notification_Apps/archive/master.zip', () => {
+                    
+                    // unzip it and delete the .zip file after
+                    unzip( "Notification_Apps-master.zip" );
+                    fs.unlinkSync( path.join( __dirname, "Notification_Apps-master.zip") );
+                    
                     fs.renameSync( path.join( __dirname, "Notification_Apps-master" ), path.join( __dirname, "Notification_Apps"));
 
                     // we're going to initially say that we are launching on startup
@@ -66,6 +69,7 @@ function checkForNewRelease( repoName, next ){
         path: "/repos/FlyingOranger/" + repoName + "/releases/latest",
         method: "GET",
         headers: {
+            // user agent is required here or we don't get a response
             "user-agent": "FlyingOranger updater checking latest release"
         }
     };
@@ -79,18 +83,44 @@ function checkForNewRelease( repoName, next ){
 
             data = JSON.parse(data);
 
+            // check release ids to see if we have a new release
             if ( data.id !== releaseInfo[repoName]){
+                
+                // we have a new release, so save this one's id
                 releaseInfo[repoName] = data.id;
                 fs.writeFileSync( path.join( __dirname, "releaseInfo.json"), JSON.stringify( releaseInfo ));
-				deleteFolder( path.join (__dirname, repoName));
-                downloader(data.zipball_url, longFileName => {
+                
+                if ( repoName === "FlyingOranger"){
                     
-                    longFileName = path.basename( longFileName, ".zip");
-                    var folderName = "FlyingOranger-" + repoName + "-" + longFileName.substr( longFileName.length - 7, longFileName.length);
-                    fs.renameSync( path.join( __dirname, folderName ), path.join( __dirname, repoName));
+                    // delete the previous release
+                    deleteFolder( path.join (__dirname, repoName));
                     
-                    next();
-                });
+                    // download the new release in a zip
+                    downloader(data.zipball_url, longFileName => {
+                        
+                        // unzip it and delete the .zip file after
+                        unzip( longFileName );
+                        fs.unlinkSync( path.join( __dirname, longFileName) );
+
+                        // rename the folder we unzipeed, which has some conveluded name
+                        // based on the git commit last 7 alphanumerics
+                        longFileName = path.basename( longFileName, ".zip");
+                        var folderName = "FlyingOranger-" + repoName + "-" + longFileName.substr( longFileName.length - 7, longFileName.length);
+                        fs.renameSync( path.join( __dirname, folderName ), path.join( __dirname, repoName));
+
+                        next();
+                    });
+                } else if ( repoName === "JavaGUI"){
+                    
+                    // get rid of the old jar
+                    var oldPath =  path.join( __dirname, "JavaGUI.jar");
+                    if (fs.existsSync( oldPath )) 
+                        fs.unlinkSync( oldPath );
+                    
+                    // our JavaGUI.jar is uploaded as an asset on our release
+                    downloader( data.assets[0].browser_download_url, next);
+                    
+                }
             }
             else next();
         });
@@ -120,24 +150,22 @@ function downloader( downloadUrl, cb ){
         }
     };
 
+    // substring to get rid of the colon after https: for example
     protocol[newPath.protocol.substr(0, newPath.protocol.length -1)].request(url_options, res => {
 
-        //console.log(res.statusCode);
-        //console.log(res.headers);
         if (res.statusCode == 302){
 
             downloader( res.headers.location, cb);
 
         } else if (res.statusCode == 200){
 
-            var filename = res.headers["content-disposition"].split("; ")[1].split("=")[1];
+            var filename = res.headers["content-disposition"].split("=")[1];
             var ws = fs.createWriteStream(filename);
 
             res.pipe(ws);
 
             ws.on('finish', () => {
-                unzip( filename );
-                fs.unlinkSync( path.join( __dirname, filename) );
+                
                 if (cb) cb( filename );
             });
 
